dffe826b3694ae17cf684507a8d74457446f0572
Matthew Cook <matthew_cook@manulife.com>
2014-11-12 15:43:53 -0500

    uploading photo will now return data, time, gps and address of photo

--- routes/receipt.js
+++ routes/receipt.js
@@ -3,46 +3,57 @@ var router = express.Router();
 var tesseract = require('node-tesseract');
 var fs = require('fs');
 var exec = require('child_process').exec;
+var google = require('googlemaps');
+var util = require('util');
 var fs = require('fs')
   , gm = require('gm');
 
-router.post('/parse', function(req, res) {
+
+  router.get('/', function(req,res) {
+    res.render('upload', { title: 'upload'})
+  })
+
+  .post('/parse', function(req, res) {
    var ex_script = __dirname+"/../scripts/ocr.sh "+__dirname+"/../"+req.files.image.path
    exec(ex_script, function(error, stdout, stderr) {
     tesseract.process(req.files.image.path, function (err,text) {
       var pretty_text = text.split("\n").join(" ");
       fs.unlink(req.files.image.path);
       console.log(pretty_text);
+      });
+    });
+  })
+
+  .post('/gps', function(req,res) {
+    gm(req.files.image.path)
+    .options({imageMagick: true})
+    .identify('%[EXIF:*GPSLongitude*]%[EXIF:*GPSLatitude*]%[EXIF:*DateTime*]', function(err,exif) {
+      fs.unlink(req.files.image.path);
+      var meta = parseMetaData(exif);
+      google.reverseGeocode(meta.gps, function(err, data){
+        meta.address = data.results[0].formatted_address;
+        res.send(meta);
+      });
     });
   });
-});
 
-router.post('/gps', function(req,res) {
-  gm(req.files.image.path)
-  .options({imageMagick: true})
-  .identify('%[EXIF:*GPSLongitude*], %[EXIF:*GPSLatitude*]', function(err,gps) {
-    fs.unlink(req.files.image.path);
-    res.send(''+parseGPS(gps));
-  });
-});
+function parseMetaData(exif) {
 
-router.get('/', function(req,res) {
-  res.render('upload', { title: 'upload'})
-});
+  var meta = exif.split("\n");
 
-function parseGPS(gps) {
-  var jgps = gps.split("\n");
+  var lon = meta[0].replace("exif:GPSLongitude=","").split(",");
+  var lat = meta[2].replace("exif:GPSLatitude=","").split(",");
 
-  var lon = jgps[0].replace("exif:GPSLongitude=","").split(",");
-  var lat = jgps[2].replace(", ",'').replace("exif:GPSLatitude=","").split(",");
-
-  var lon_d = jgps[1].replace("exif:GPSLongitudeRef=","");
-  var lat_d = jgps[3].replace("exif:GPSLatitudeRef=","");
+  var lon_d = meta[1].replace("exif:GPSLongitudeRef=","");
+  var lat_d = meta[3].replace("exif:GPSLatitudeRef=","");
 
   var dd_lat = DMStoDD(lat,lat_d);
   var dd_lon = DMStoDD(lon,lon_d);
 
-  return dd_lat+", "+dd_lon;
+  var time = meta[4].replace("exif:DateTime=","");
+
+  var data = { gps: dd_lat+", "+dd_lon, time: time };
+  return data;
 }
 
 function DMStoDD(dms, direction) {
